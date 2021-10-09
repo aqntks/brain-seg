af5009dbd05ef5b2847b365e8128f805105311f4
aqntks <cjd0030@gmail.com>
2021-10-09 15:20:42 +0900

    add brain visualization function

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import numpy as np
 import pandas as pd
 import nibabel as nib
-
+import os
 import cv2
 
 from util import *
 import matplotlib.pyplot as plt
+
+
+# 구조적 시각화
+def visual(data):
+    fig, ax = plt.subplots(1, 6, figsize=[18, 3])
+    n = 0
+    slice = 0
+    for _ in range(6):
+        ax[n].imshow(data[:, :, slice], 'gray')
+        ax[n].set_xticks([])
+        ax[n].set_yticks([])
+        ax[n].set_title('Slice number: {}'.format(slice), color='r')
+        n += 1
+        slice += 10
+
+    fig.subplots_adjust(wspace=0, hspace=0)
+    plt.show()
 
 
 def load_case(image_nifty_file, label_nifty_file):
@@ -18,8 +35,12 @@ def load_case(image_nifty_file, label_nifty_file):
 def brain(DATA_DIR):
     image, label = load_case(DATA_DIR + "imagesTr/BRATS_003.nii.gz", DATA_DIR + "labelsTr/BRATS_003.nii.gz")
 
-    cv2.imshow('333')
-    cv2.waitKey(0)
+    img = nib.load(DATA_DIR + "imagesTr/BRATS_003.nii.gz")
+    img_data = img.get_fdata()
+
+    print(img_data.shape)
+    visual(img_data)
+
 # slice_0 = image[26, :, :, 0]
     # slice_1 = image[:, 30, :, 0]
     # slice_2 = image[:, :, 16, 0]
